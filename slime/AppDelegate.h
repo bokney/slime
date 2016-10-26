8c3cd95e76ec2142e685e4c13fc087d3ee45a310
bokny <bokny@boknys-mac-mini.home>
2016-10-26 23:48:01 +0100

    Initial Commit

---
+++ slime/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  slime
+//
+//  Created by bokny on 26/10/2016.
+//  Copyright © 2016 bokny. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ slime/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  slime
+//
+//  Created by bokny on 26/10/2016.
+//  Copyright © 2016 bokny. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
